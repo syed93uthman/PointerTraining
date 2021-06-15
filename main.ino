6077956f512475c5e36497b2f677258b9bb348eb
syed93uthman <syed_uthman@yahoo.com>
2021-06-15 16:15:24 +0800

    I create DataOutput & Input

--- main.ino
+++ main.ino
@@ -1,9 +1,25 @@
-int valA;
-int valB;
-int result1 = 0;
-int result2 = 0;
-int result3 = 0;
-int result4 = 0;
+
+struct DataInput
+{
+    int valA;
+    int valB;
+};
+
+struct DataOutput
+{
+    int result1 = 0; // A+B
+    int result2 = 0; // B-A
+    int result3 = 0; // B+B
+    int result4 = 0; // A+A
+} myOutput;
+
+// set valA & B   ( 7 , 10)
+// setValA&B (int A,int B,*DataInput)
+// prcoess(DataInput , *DataOutput)
+// printData value
+
+DataInput myInput1;
+DataInput myInput2;
 
 void setup()
 {
@@ -12,4 +28,22 @@ void setup()
 
 void loop()
 {
+}
+
+void setValA_B(int A, int B, DataInput *input)
+{
+    input->valA = A;
+    input->valB = B;
+}
+
+void process(DataInput input, DataOutput *output)
+{
+    output->result1 = input.valA + input.valB;
+    //
+}
+
+void printData(DataOutput output)
+{
+    Serial.println(output.result1);
+    //
 }
