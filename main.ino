af683c273d77847baf9ddbb340263c36f0600655
farhanhakimi30 <farhanhakimianuar@gmail.com>
2021-06-16 13:22:52 +0800

    non-simplify v2

--- main.ino
+++ main.ino
@@ -1,13 +1,13 @@
 ////////// Non-Simplify ///////////////
 
-#include "src/formula1.h"
+ #include "src/formula1.h"
 
 formula1 run;
 
 void setup()
     {
         Serial.begin(9600);
-        run.begin();
+        run.begin(7, 10);
     }
 
 void loop() //empty, only run once
--- src/formula1.h
+++ src/formula1.h
@@ -1,43 +1,45 @@
 #pragma once
-class formula1 //non-simplify
 
-{
-private:
-    struct DataInput //using struct
+struct DataInput //using struct
         {
              int valA; //set no value
              int valB;
-        }myInput;
+        }myInput; //nama object
             
-    struct DataOutput
+struct DataOutput
         {
              int result1 = 0; //A+B
              int result2 = 0; //B-A
              int result3 = 0; //B+B
              int result4 = 0; //A+A
-         }myOutput;
+        }myOutput;
+
+class formula1 //non-simplify
+{
+private:
+    
 public:
     formula1()
     {
 
     };  
 
-    void begin()
+    void begin(int a, int b)
     {
-        setValA_B (7 , 10, &myInput);
+        setValA_B (a , b, &myInput);
         process(myInput,&myOutput);
         printData(myOutput);
     }
      
-     void setValA_B(int A, int B, DataInput *input) //step 1
+     void setValA_B(int A, int B, DataInput *input) //step 1 , dptkan value dari luar
     {
-        input->valA = A; // access structure sebagai pointer
+        input->valA = A; // access structure sebagai pointer , -> ialah address pointer
         input->valB = B;
     }
 
     void process(DataInput input, DataOutput *output) // step 2 _ input ialah object, bukan pointer '*'
     {
-        output->result1 = input.valA + input.valB;
+        output->result1 = input.valA + input.valB; //input.valA, guna . sbb xde * dan nk ambil value, kalau ada *, guna ->
         output->result2 = input.valB - input.valA;
         output->result3 = input.valB + input.valB;
         output->result4 = input.valA + input.valA;
@@ -45,7 +47,7 @@ public:
 
     void printData(DataOutput output) // Step 3_ display output
     {
-        Serial.println(output.result1);
+        Serial.println(output.result1); // . ambil value sahaja, xde ganti.
         Serial.println(output.result2);
         Serial.println(output.result3);
         Serial.println(output.result4);
